c19e49486b836353c8963399053c954b3a079185
Золотарёв <vaceslavz922@gmail.com>
2025-10-15 15:12:21 +0500

    Add files via upload

---
+++ Laba_float_0.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+void bin(unsigned int n) {
+    int bit = sizeof(unsigned int) * 8;
+    for (int i = bit - 1; i >= 0; i--) {
+        cout << ((n >> i) & 1);
+    }
+    cout << endl;
+}
+int main() {
+    unsigned int num;
+    cin >> num;
+    bin(num);
+    return 0;
+}
