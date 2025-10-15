cae9a41a4c7ca5b94bd3fecff922211819698c71
Золотарёв <vaceslavz922@gmail.com>
2025-10-15 15:16:03 +0500

    Add files via upload

---
+++ Laba_float_1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+union fu {
+    float f;
+    unsigned int u;
+};
+void bin(unsigned int u) {
+    for (int i = 31; i >= 0; i--) {
+        cout << ((u >> i) & 1);
+    }
+    cout << endl;
+}
+int main() {
+    fu a_lab_rat;
+    cin >> a_lab_rat.f;
+    bin(a_lab_rat.u);
+}
