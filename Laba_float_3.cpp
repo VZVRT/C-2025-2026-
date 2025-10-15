b4ccfc72e8600d1e35a09470d0878098f7d032d3
Золотарёв <vaceslavz922@gmail.com>
2025-10-15 15:28:02 +0500

    Add files via upload

---
+++ Laba_float_3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	cout << fixed;
+	cout.precision(2);
+	float n = 1664813248426.1f;
+	int c = 0;
+	while (n < 1664813248428.5){
+		n += 1.0f;
+		if (c % 1000000 == 0) {
+			cout << n << endl;
+		}
+		c++;
+	}
+	return 0;
+}
